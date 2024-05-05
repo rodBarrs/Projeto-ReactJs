01d4fe9a2f0136e113a7450cc6196d98e2e386df
rodBarrs <rodrigosbarros51@gmail.com>
2024-05-05 16:25:57 -0300

    adicionando Evento onChange

--- src/index.js
+++ src/index.js
@@ -12,11 +12,19 @@ class MeuComponente extends React.Component{
         alert('Estou fazendo o curso de ' + curso);
     } 
 
+    teclado = (obj) =>{
+        console.log(obj.target.value);
+    }
+
     render(){
         return(
             <div>
                 <button onClick={this.mensagem.bind(this,'Rodrigo')}>Clique aqui</button>
                 <button onClick={() => this.minhaArorowFunction('ReactJS')}> Arrow Function </button>
+
+                <hr />
+                <input type='text' onChange={this.teclado} />
+
             </div>
         );
     }
